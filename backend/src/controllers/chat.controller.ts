322fdf539401da89352af826acbfd73381b8588d
Nakul443 <nakul.kumar627@gmail.com>
2026-06-05 05:36:04 +0000

    send message to a chat session API implemented

--- backend/src/controllers/chat.controller.ts
+++ backend/src/controllers/chat.controller.ts
@@ -22,3 +22,53 @@ export const startChat = async (req: Request, res: Response, next: NextFunction)
     next(error); // Passes any DB errors to your global error handler in index.ts
   }
 };
+
+// POST /api/chat/message
+export const sendMessage = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const { chatId, content } = req.body;
+
+    if (!chatId || !content) {
+      res.status(400).json({ error: 'Missing required fields: chatId and content are required.' });
+      return;
+    }
+
+    // verify that the chat session actually exists
+    const chatExists = await prisma.chat.findUnique({
+      where: { id: chatId },
+    });
+
+    if (!chatExists) {
+      res.status(404).json({ error: 'Chat session not found.' });
+      return;
+    }
+
+    // save the User's incoming message to the database
+    const userMessage = await prisma.message.create({
+      data: {
+        chatId,
+        content,
+        sender: 'USER',
+      },
+    });
+
+    // ai response to be added
+
+    // save the AI's response to the database
+    // const aiMessage = await prisma.message.create({
+    //   data: {
+    //     chatId,
+    //     content: mockAiResponse,
+    //     sender: 'AI',
+    //   },
+    // });
+
+    res.status(200).json({
+      success: true,
+      userMessage,
+    //   aiMessage,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- backend/src/routes/chat.routes.ts
+++ backend/src/routes/chat.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
-import { startChat} from '../controllers/chat.controller.js';
+import { startChat, sendMessage } from '../controllers/chat.controller.js';
 
 const router = Router();
 
 // 1. start a new chat session
 router.post('/start', startChat);
+
+// 2. send a message in the chat
+router.post('/message', sendMessage);
+
+
 
 export default router;
